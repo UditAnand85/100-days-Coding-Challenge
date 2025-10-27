937bdfb67a361174e353f9edc48ffd869c9985ef
UditAnand85 <143195427+UditAnand85@users.noreply.github.com>
2025-10-28 01:22:45 +0530

    Add files via upload

---
+++ Day1/areaofcircle.py
@@ -0,0 +1,8 @@
+import math
+class Solution:
+    def calculateArea(self, r):
+        return math.pi * r * r
+    
+
+sol = Solution()
+print(sol.calculateArea(5))
---
+++ Day1/leapyear.py
@@ -0,0 +1,9 @@
+class Solution:
+    def checkYear (self, n):
+        if n%4 == 0 and n%100 != 0:
+            return True
+        else:
+            if n % 400 == 0:
+                return True
+            else:
+                return False
