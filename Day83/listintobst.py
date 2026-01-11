41b022104846d7debcdbd93eec2f6d68ae064900
UditAnand85 <143195427+UditAnand85@users.noreply.github.com>
2026-01-11 19:36:10 +0530

    Add files via upload

---
+++ Day83/listintobst.py
@@ -0,0 +1,35 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def sortedListToBST(self, head):
+        def findMiddle(head):
+            prev = None
+            slow = head
+            fast = head
+            while fast and fast.next:
+                prev = slow
+                slow = slow.next
+                fast = fast.next.next
+            if prev:
+                prev.next = None
+            return slow
+
+        if not head:
+            return None
+        if not head.next:
+            return TreeNode(head.val)
+
+        mid = findMiddle(head)
+        root = TreeNode(mid.val)
+        root.left = self.sortedListToBST(head)
+        root.right = self.sortedListToBST(mid.next)
+        return root
