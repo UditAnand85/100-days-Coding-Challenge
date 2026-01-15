b7ea1a6d3ac8583d2b560fb1dff6d372925ec76d
UditAnand85 <143195427+UditAnand85@users.noreply.github.com>
2026-01-15 18:40:53 +0530

    Add files via upload

---
+++ Day87/insertionsortlist.py
@@ -0,0 +1,24 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def insertionSortList(self, head):
+        dummy = ListNode(0)
+        curr = head
+
+        while curr:
+            prev = dummy
+            next_node = curr.next
+
+            while prev.next and prev.next.val < curr.val:
+                prev = prev.next
+
+            curr.next = prev.next
+            prev.next = curr
+            curr = next_node
+
+        return dummy.next
+
+        
